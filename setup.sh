a0d3f33b91495a167544649000ccaeeece3fa5b1
Kit Corey <kitcorey@gmail.com>
2017-02-09 18:51:28 -0700

    Add files for git and create a setup script

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+usage() { echo "Usage: $0 [-o force overwrite]" 1>&2; exit 1; }
+
+setup_vim() {
+    destination=~/.vim/bundle/Vundle.vim
+    if [ ! -d "$destination" ] ; then
+        git clone https://github.com/tmux-plugins/tpm $destination
+    fi
+    cp $cp_options vimrc ~/.vimrc
+}
+
+setup_tmux() {
+    destination=~/.tmux/plugins/tpm
+    if [ ! -d "$destination" ] ; then
+        git clone https://github.com/tmux-plugins/tpm $destination
+    fi
+    cp $cp_options tmux.conf ~/.tmux.conf
+}
+
+setup_git() {
+    cp $cp_options gitignore ~/.gitignore
+    gitignore=~/.gitignore
+    git config --global core.excludesfile $gitignore
+}
+
+
+cp_options="--no-clobber"
+while getopts ":o" o; do
+    case "${o}" in
+        o)
+            echo "HI"
+            cp_options=""
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+setup_vim
+setup_tmux
+setup_git
