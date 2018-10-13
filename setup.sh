b3daadf456ca3accfca82e5b92dde8a9c82ce791
Kit Corey <kitcorey@users.noreply.github.com>
2018-10-13 07:09:40 -0600

    Add whatismyip to aliases and improve fzf setup

--- setup.sh
+++ setup.sh
@@ -79,7 +79,7 @@ setup_repos() {
 
 setup_fzf() {
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --bin
+    cd ~/.fzf && git pull && ./install --bin && cd -
 }
 
 append_with_newline() {
--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -2,3 +2,5 @@ myipv4() {
     ifconfig enp12s0 | grep "inet " | awk -F'[: ]+' '{ print $4 }'
 }
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
+alias whatismyip="curl ifconfig.co"
+
