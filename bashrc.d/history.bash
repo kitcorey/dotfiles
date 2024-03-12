ebe1cd631c53c8c746ff58ffde3b7e6a121251e1
Kit Corey <kit.corey@maxar.com>
2024-03-12 14:10:34 -0600

    Add history.bash

---
+++ bashrc.d/history.bash
@@ -0,0 +1,10 @@
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+export HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+export HISTSIZE=10000
+export HISTFILESIZE=20000
