1a29e2cb23d704a237f148ea5584a8bcbd1a4e75
Kit Corey <kit.corey@maxar.com>
2025-02-10 14:40:10 -0700

    Show relative filepath alongside file in lualine
    Use relative path in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,21 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        sections = {
+            lualine_c = {
+                {
+                    "filename",
+                    newfile_status = false,  -- Display new file status (new file means no write after created)
+                    path = 1       -- 0: Just the filename
+                                   -- 1: Relative path
+                                   -- 2: Absolute path
+                                   -- 3: Absolute path, with tilde as the home directory
+                                   -- 4: Filename and parent dir, with tilde as the home directory
+                }
+            }
+        }
+    },
     config = function(_, opts)
         require("lualine").setup(opts)
     end
