a3997848572b7a351a9ff00e914c865d8ec4c460
Kit Corey <kit.corey@maxar.com>
2025-03-03 18:14:32 -0700

    Disable change_detection notify for Lazy.vim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,11 +25,16 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    {
+        import = "plugins",
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  change_detection = {
+      notify = false,
+  },
 })
