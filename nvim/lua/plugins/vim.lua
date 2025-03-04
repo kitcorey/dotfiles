4f10966cbe36af6c2a89161a5944cba7509f1612
Kit Corey <kit.corey@maxar.com>
2025-03-03 18:11:57 -0700

    Add vim.lua to Lazy.vim plugins

---
+++ nvim/lua/plugins/vim.lua
@@ -0,0 +1,21 @@
+return {
+    {'nvie/vim-flake8'},
+    {
+        'xolox/vim-notes',
+        dependencies = { 'xolox/vim-misc' }
+    },
+    {'AndrewRadev/linediff.vim'},
+    {'ludovicchabant/vim-lawrencium'},
+    {'tpope/vim-fugitive'},
+    {'godlygeek/tabular'},
+    {'chazy/cscope_maps'},
+    {'tpope/vim-abolish'},
+    {'chrisbra/csv.vim'},
+    {'andymass/vim-matchup'},
+    {'elzr/vim-json'},
+    {'vim-scripts/repmo.vim'},
+    {'PProvost/vim-ps1'},
+    {'tpope/vim-unimpaired'},
+    {'tommcdo/vim-lion'},
+    {'AndrewRadev/sideways.vim'}
+}
