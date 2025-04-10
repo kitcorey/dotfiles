aa02336780300f16f1c201b0d8840c538c0279b1
Kit Corey <kit.corey@maxar.com>
2025-04-10 14:11:43 -0600

    Make fzf preview fullscreen

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,25 +5,27 @@ return {
             -- optional for icon support
             "nvim-tree/nvim-web-devicons"
         },
-		opts = {
-			"default",
-			winopts = {
-				preview = {
-					hidden = false,
-					default = "bat"
-				}
-			},
-			previewers = {
-				bat = {
-					args = "--color=always --style=numbers,changes --theme=gruvbox-dark"
-				}
-			}
-		},
-	    config = function(_, opts)
-	        require("fzf-lua").setup(opts)
+        opts = {
+            "default",
+            winopts = {
+                fullscreen = true,
+                preview = {
+                    hidden = false,
+                    default = "bat",
+                    flip_columns   = 150 -- #cols to switch to horizontal on flex
+                }
+            },
+            previewers = {
+                bat = {
+                    args = "--color=always --style=numbers,changes --theme=gruvbox-dark"
+                }
+            }
+        },
+        config = function(_, opts)
+            require("fzf-lua").setup(opts)
             vim.keymap.set('n', '<C-p>', require('fzf-lua').files)
             vim.keymap.set('n', '<space>/', require('fzf-lua').grep)
             vim.keymap.set('n', '<space>b', require('fzf-lua').buffers)
-	    end
+        end
     }
 }
