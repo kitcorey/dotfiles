32fd9b3423a6a09c9bcd34df6b35503100597721
Kit Corey <kitcorey@users.noreply.github.com>
2025-06-08 08:11:56 -0600

    Update neovim zshrc config

--- zshrc.d/nvim.zsh
+++ zshrc.d/nvim.zsh
@@ -1,5 +1,8 @@
-if ! command -v nvim 2>&1 >/dev/null
-then
-    PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-fi
+# Installation of Neovim is managed by Homebrew.
+#if ! command -v nvim 2>&1 >/dev/null
+#then
+#    PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+#fi
+
 alias vi=nvim
+export EDITOR="nvim"
