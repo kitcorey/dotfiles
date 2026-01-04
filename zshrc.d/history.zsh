c421cbd1e60bfd29ef8a7d080d103398c9e105dd
Kit Corey <kitcorey@users.noreply.github.com>
2026-01-04 15:17:59 +0000

    Improve zsh history handling

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -6,8 +6,14 @@
 # Always append history, don't overwrite it
 # Store multiline commands as a single command
 #
-export HISTCONTROL=ignoredups:erasedups # no duplicate entries
+#export HISTCONTROL=ignoredups:erasedups # no duplicate entries
 export HISTSIZE=100000                   # custom history size
 export HISTFILESIZE=100000              # custom history file size
-setopt HIST_IGNORE_ALL_DUPS
+export SAVEHIST=100000
+export HISTFILE=~/.zsh_history
+setopt appendhistory      # Append history lines to the file (instead of overwriting)
+setopt share_history      # Share history among all open zsh sessions
+setopt extended_history   # Record the time when each command was executed
+setopt hist_ignore_all_dups   # Do not enter a command into the history list if it is a duplicate of the previous one
+
 #PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
